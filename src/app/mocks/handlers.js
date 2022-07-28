9a374325bc445f1a3030de83caf1d1213fb1a2f6
Jon Rutter <contact@jonrutter.io>
2022-07-28 16:45:30 -0400

    Add time delay to msw handlers

--- src/app/mocks/handlers.js
+++ src/app/mocks/handlers.js
@@ -4,7 +4,11 @@ import { mockLocationData } from './mock-data/mock-location-data';
 
 export const handlers = [
   rest.get('/api/location', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ ...mockLocationData }));
+    return res(
+      ctx.delay(2000),
+      ctx.status(200),
+      ctx.json({ ...mockLocationData })
+    );
   }),
   rest.get('/api/weather', null),
 ];
