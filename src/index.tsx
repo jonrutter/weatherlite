afee72e4e502e73f05d2a65b6e48c704fe83846d
Jon Rutter <contact@jonrutter.io>
2022-07-29 19:02:29 -0400

    Suppress unneeded msw browser warnings

--- src/index.tsx
+++ src/index.tsx
@@ -40,7 +40,9 @@ import './styles/index.css';
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./app/mocks/browser');
-  worker.start();
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 ReactDOM.render(
