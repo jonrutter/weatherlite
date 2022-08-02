cb6b6e5fd7f3e909a3caef872de58079468cbaa1
Jon Rutter <contact@jonrutter.io>
2022-08-02 14:18:44 -0400

    Add msw handler for weather api

--- src/app/mocks/handlers.js
+++ src/app/mocks/handlers.js
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 
 import { mockLocationData } from './mock-data/mock-location-data';
+import { mockWeatherData } from './mock-data/mock-weather-data';
 
 export const handlers = [
   rest.get('/api/location', (req, res, ctx) => {
@@ -10,5 +11,11 @@ export const handlers = [
       ctx.json({ ...mockLocationData })
     );
   }),
-  rest.get('/api/weather', null),
+  rest.get('/api/weather', (req, res, ctx) => {
+    return res(
+      ctx.delay(2000),
+      ctx.status(200),
+      ctx.json({ ...mockWeatherData })
+    );
+  }),
 ];
