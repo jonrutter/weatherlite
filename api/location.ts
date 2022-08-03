5c1ae71ca6ca4eb9e602452ed4f3d16f0d65e0d5
Jon Rutter <contact@jonrutter.io>
2022-08-03 16:36:36 -0400

    Document serverless functions

--- api/location.ts
+++ api/location.ts
@@ -10,13 +10,14 @@ const options = {
 };
 
 /**
- *
+ * A serverless function through which all geolocation api requests are routed.
  */
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
   const { q: query } = request.query;
+  // send error response to client if query is missing
   if (!query) {
     return response.status(400).send({
       message: 'Improperly configured request. Missing search query.',
@@ -26,14 +27,17 @@ export default async function handler(
       const endpoint = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=10000&sort=population&limit=10&namePrefix=${query}`;
       const res = await fetch(endpoint, options);
       const data = await res.json();
+      // throw error if there is an error response from GeoDB
       if (res.status >= 400)
         throw new Error(
           `There was an error retrieving the location data from GeoDB: error ${res.status}, ${data.message}`
         );
+      // otherwise return response
       return response.status(200).json({ ...data });
     } catch (err) {
       let message = 'Unknown Error';
-      if (err instanceof Error) message = err.message;
+      if (err instanceof Error) message = err.message; // handling TS typing err as unknown by default
+      // if an error was thrown anywhere in fetching/processing data from GeoDB, send an error response to the client
       return response.status(400).json({ message });
     }
   }
--- api/weather.ts
+++ api/weather.ts
@@ -3,11 +3,15 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
+/**
+ * A serverless function through which all geolocation api requests are routed.
+ */
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
   const { lat, lon } = request.query;
+  // send error response to client if lat or lon are missing
   if (!lat || !lon) {
     response.status(400).send({
       message:
@@ -25,15 +29,18 @@ export default async function handler(
           `There was an error retrieving the weather data from Openweathermap: error ${res.status}, ${data?.message}`
         );
       else if (data?.cod && data.cod >= 400) {
-        // when openweathermap encounters a problem, it sometimes sends an error response with an http status of 200. this checks to make sure that the response is not one of these errors
+        // when OpenWeatherMap encounters a problem, it sometimes still sends an http 200 response (but with a response body indicating that there was an error).
+        // so this code checks to make sure that the response is not one of these errors
         throw new Error(
           `There was an error retrieving the weather data from Openweathermap: error code ${data?.cod}, ${data?.message}`
         );
       }
+      // otherwise return data
       return response.status(200).json({ ...data });
     } catch (err) {
       let message = 'Unknown Error';
-      if (err instanceof Error) message = err.message;
+      if (err instanceof Error) message = err.message; // handling TS typing err as unknown by default
+      // if an error was thrown anywhere in fetching/processing data from OpenWeatherMap, send an error response to the client
       return response.status(400).json({ message });
     }
   }
