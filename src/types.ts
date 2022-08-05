0344dfeda65428a36dbbb2def0850833e9f63e4a
Jon Rutter <contact@jonrutter.io>
2022-08-05 16:24:57 -0400

    Add app types
    - Add docs for app types
    
    - Add types defs for WeatherEvent, TimeOfDay, WeatherCase

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,70 @@
+/*
+App Data Types
+*/
+
+/*
+Reference: 
+
+There are two main types of data that affect the UI of this weather app:
+- Times of day
+- Weather events
+
+Times of day can be: 'day', 'night', or 'twilight'.
+
+Weather events are discrete types of weather (e.g., 'snow', 'rain')
+
+OpenWeatherMap API returns a weather 'code' for each type of weather condition that it recognizes (reference: https://openweathermap.org/weather-conditions). There are dozens of different weather codes, which represent weather conditions with a much higher degree of granularity than this weather app needs (e.g., 'light intensity drizzle', 'drizzle', 'heavy intensity drizzle', etc.).
+
+So this weather app defines its weather events as ranges of OpenWeatherMap codes.
+
+For example: OpenWeatherMap recognizes ten different types of Thunderstorm, with codes ranging from 200 to 232 (all codes from 200-299 are reserved for thunderstorms). This app defines a single 'thunderstorm' weather event, which represents all OpenWeatherMap codes from 200 to 299.
+
+---
+
+The current weather event and the time of day affect the appearance of the UI in terms of its color scheme, as well as the main weather image
+
+---
+
+This app decides what UI to implement by using 'cases', which are objects containing either/both 'times' and 'weather' properties (are arrays of TimeOfDay and WeatherEvent, respectively). By creating a mapping of a 'color' or 'image' to a list of cases, the app can use a single function to evaluate whether a given time/event pairing are reflected in any of the cases, and return the correct image or color combination.
+*/
+
+/**
+ * Maps weather events to OpenWeatherMap code ranges
+ */
+export const weatherEvents = {
+  heavyClouds: [[804]],
+  clouds: [[803]],
+  lightClouds: [[801, 802]],
+  clear: [[800]],
+  atmospheric: [[700, 799]],
+  snow: [
+    [600, 610],
+    [617, 699],
+  ],
+  hail: [[611, 613]],
+  winterMix: [[511], [614, 616]],
+  rain: [
+    [300, 399],
+    [500, 510],
+    [512, 599],
+  ],
+  thunderstorm: [[200, 299]],
+  rainbow: [[100]],
+};
+
+export type WeatherEvent = keyof typeof weatherEvents;
+
+export type TimeOfDay = 'day' | 'night' | 'twilight';
+
+export type WeatherCase = {
+  weather?: WeatherEvent[];
+  time?: TimeOfDay[];
+};
+
+/*
+Location API responses and data
+*/
+
 export type Nullable<T> = T | undefined | null;
 
 export type Location = {
@@ -35,7 +102,9 @@ type UnsuccessfulGeoResponse = {
 
 export type GeoResponse = LocationQuery | UnsuccessfulGeoResponse;
 
-// Weather types
+/*
+Weather API responses and data
+*/
 
 type WeatherStump = {
   id: Nullable<number>;
@@ -149,5 +218,3 @@ type UnsuccessfulWeatherResponse = {
 };
 
 export type WeatherResponse = WeatherData | UnsuccessfulWeatherResponse;
-
-export type TimeOfDay = 'day' | 'night' | 'twilight';
