d13313a2f597334fa22adb36294d909d77692fd1
Jon Rutter <contact@jonrutter.io>
2022-08-05 18:06:46 -0400

    Create weather icon component
    - Create component for rendering weather icons
    
    - Add tests

---
+++ src/components/__tests__/icon.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+// component
+import { Icon } from '../icon';
+
+describe('Icon', () => {
+  it('renders icon of thunderstorm', () => {
+    render(<Icon code={200} timeOfDay="day" />);
+    screen.getByTestId('thunderstorm');
+  });
+  it('renders icon of rain', () => {
+    render(<Icon code={500} timeOfDay="day" />);
+    screen.getByTestId('rain');
+  });
+  it('renders icon of winter mix', () => {
+    render(<Icon code={511} timeOfDay="day" />);
+    screen.getByTestId('winter mix');
+  });
+  it('renders icon of snow', () => {
+    render(<Icon code={600} timeOfDay="day" />);
+    screen.getByTestId('snow');
+  });
+  it('renders icon of hail', () => {
+    render(<Icon code={611} timeOfDay="day" />);
+    screen.getByTestId('hail');
+  });
+  it('renders icon of atmospheric weather', () => {
+    render(<Icon code={701} timeOfDay="day" />);
+    screen.getByTestId('atmospheric');
+  });
+  it('renders icon of clear sun', () => {
+    render(<Icon code={800} timeOfDay="day" />);
+    screen.getByTestId('sun');
+  });
+  it('renders icon of clear moon', () => {
+    render(<Icon code={800} timeOfDay="night" />);
+    screen.getByTestId('moon');
+  });
+  it('renders icon of cloudy sun', () => {
+    render(<Icon code={801} timeOfDay="day" />);
+    screen.getByTestId('clouds with sun');
+  });
+  it('renders icon of cloudy moon', () => {
+    render(<Icon code={801} timeOfDay="night" />);
+    screen.getByTestId('clouds with moon');
+  });
+  it('renders icon of light clouds', () => {
+    render(<Icon code={803} timeOfDay="day" />);
+    screen.getByTestId('light clouds');
+  });
+  it('renders icon of heavy clouds', () => {
+    render(<Icon code={804} timeOfDay="day" />);
+    screen.getByTestId('heavy clouds');
+  });
+
+  it('renders with invalid input', () => {
+    render(<Icon code={100} timeOfDay="day" />);
+    screen.getByTestId('alien');
+  });
+});
---
+++ src/components/icon.tsx
@@ -0,0 +1,132 @@
+import React, { useMemo } from 'react';
+
+// types
+import type { TimeOfDay, WeatherCase, WeatherEvent } from '../types';
+
+// util funcs
+import { hasCase, codeToWeatherEvent } from '../utils/weather';
+
+// icons
+import {
+  WiWindy,
+  WiHail,
+  WiCloudy,
+  WiCloud,
+  WiRainMix,
+  WiNightAltCloudy,
+  WiNightClear,
+  WiRain,
+  WiSnow,
+  WiDayCloudy,
+  WiDaySunny,
+  WiThunderstorm,
+  WiAlien,
+} from 'react-icons/wi';
+import { IconType } from 'react-icons';
+
+type IconDetails = {
+  icon: IconType;
+  name: string;
+  cases: WeatherCase[];
+};
+/**
+ * Array of objects mapping a weather icon to weather cases.
+ */
+const icons: IconDetails[] = [
+  {
+    icon: WiThunderstorm,
+    name: 'thunderstorm',
+    cases: [{ weather: ['thunderstorm'] }],
+  },
+  {
+    icon: WiRain,
+    name: 'rain',
+    cases: [{ weather: ['rain'] }],
+  },
+  {
+    icon: WiRainMix,
+    name: 'winter mix',
+    cases: [{ weather: ['winterMix'] }],
+  },
+  {
+    icon: WiSnow,
+    name: 'snow',
+    cases: [{ weather: ['snow'] }],
+  },
+  { icon: WiHail, name: 'hail', cases: [{ weather: ['hail'] }] },
+  { icon: WiWindy, name: 'atmospheric', cases: [{ weather: ['atmospheric'] }] },
+  {
+    icon: WiDaySunny,
+    name: 'sun',
+    cases: [{ weather: ['clear'], time: ['day', 'twilight'] }],
+  },
+  {
+    icon: WiNightClear,
+    name: 'moon',
+    cases: [{ weather: ['clear'], time: ['night'] }],
+  },
+  {
+    icon: WiDayCloudy,
+    name: 'clouds with sun',
+    cases: [{ weather: ['lightClouds'], time: ['day', 'twilight'] }],
+  },
+  {
+    icon: WiNightAltCloudy,
+    name: 'clouds with moon',
+    cases: [{ weather: ['lightClouds'], time: ['night'] }],
+  },
+  { icon: WiCloud, name: 'light clouds', cases: [{ weather: ['clouds'] }] },
+  {
+    icon: WiCloudy,
+    name: 'heavy clouds',
+    cases: [{ weather: ['heavyClouds'] }],
+  },
+];
+
+// prop types
+interface Props extends React.ComponentPropsWithoutRef<'svg'> {
+  code: number;
+  timeOfDay: TimeOfDay;
+  className?: string;
+}
+
+// util funcs
+const findIcon = (
+  time: TimeOfDay,
+  event: WeatherEvent
+): { src: IconType; name: string } => {
+  const selectedIcon = icons.find((icon) => hasCase(icon.cases, time, event));
+  // return selectedIcon?.icon {src: selectedIcon.icon, name: selectedIcon.name} : { src: WiAlien, name: 'alien'};
+  return selectedIcon?.icon
+    ? { src: selectedIcon.icon, name: selectedIcon.name }
+    : { src: WiAlien, name: 'alien' };
+};
+
+/**
+ * Renders a weather icon based on the current weatherCode and timeOfDay
+ */
+export const Icon: React.FC<Props> = ({
+  code,
+  timeOfDay,
+  className = '',
+  ...rest
+}) => {
+  const weatherEvent = useMemo<WeatherEvent | null>(() => {
+    return codeToWeatherEvent(code);
+  }, [code]);
+
+  if (!weatherEvent) return null;
+
+  const icon = findIcon(timeOfDay, weatherEvent);
+
+  const Tag = icon.src;
+
+  return (
+    <Tag
+      data-testid={icon.name}
+      className={'max-w-full h-auto pointer-events-none'}
+      aria-label={icon.name}
+      {...rest}
+    />
+  );
+};
