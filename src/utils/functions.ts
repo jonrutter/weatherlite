9ec550b95cea2e17de49fd82c1324d5df22440b3
Jon Rutter <contact@jonrutter.io>
2022-08-05 16:26:49 -0400

    Improve util func type safety
    - Typecheck args of `capitalize()` and `toFixedZero()`

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,18 @@
 export const capitalize = (str: string): string => {
-  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  if (typeof str !== 'string') {
+    console.warn('Bad input passed to capitalize()');
+    return '';
+  } else if (str.length < 2) return str.toUpperCase();
+  else {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  }
 };
 
 export const toFixedZero = (num: number): number => {
-  return Number(num.toFixed(0));
+  if (typeof num !== 'number') {
+    console.warn('Bad input passed to toFixedZero()');
+    return 0;
+  } else {
+    return Number(num.toFixed(0));
+  }
 };
