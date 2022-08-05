888ed6d12b536e7ae947c8137fb9a9bbc9c0c5a4
Jon Rutter <contact@jonrutter.io>
2022-08-05 16:48:48 -0400

    Add a11y-bg util class to Bubble

--- src/components/bubble.tsx
+++ src/components/bubble.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Bubble: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
-  <div className="shadow-lg bg-white/30 w-[4.5rem] h-[4.5rem] flex flex-col items-center justify-center rounded-full">
+  <div className="shadow-lg w-[4.5rem] h-[4.5rem] flex flex-col items-center justify-center rounded-full bg-a11y">
     {children}
   </div>
 );
