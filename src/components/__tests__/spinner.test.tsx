daaa6b1d0d44a8da0189bc047bbcbd4d570c358d
Jon Rutter <contact@jonrutter.io>
2022-08-09 13:45:58 -0400

    Update spinner
    - Create sizes map to easily convert sizes to classNames
    
    - Add test for checking size styling

--- src/components/__tests__/spinner.test.tsx
+++ src/components/__tests__/spinner.test.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 // component
-import { Spinner } from '../spinner';
+import { Spinner, sizes } from '../spinner';
 
 describe('Spinner', () => {
   it('renders the content correctly', () => {
     render(<Spinner />);
     screen.getByTestId('loading-spinner');
     screen.getByText(/loading/i); // an accessible label should be in the document
+  });
+  it('correctly renders with a size', () => {
+    render(<Spinner size="sm" />);
+    expect(screen.getByTestId('loading-spinner')).toHaveClass(sizes.sm);
   });
 });
--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -4,22 +4,20 @@ type Props = {
   size?: 'sm' | 'md' | 'lg';
 };
 
+export const sizes = {
+  sm: 'w-[2rem] h-[2rem]',
+  md: 'w-[3rem] h-[3rem]',
+  lg: 'w-[5rem] h-[5rem]',
+};
+
 /**
  * Renders a spinner component to display during loading times.
  */
-export const Spinner: React.FC<Props> = ({ size = 'lg' }) => {
-  const diameter =
-    size === 'sm'
-      ? 'w-[2rem] h-[2rem]'
-      : size === 'md'
-      ? 'w-[3rem] h-[3rem]'
-      : 'w-[5rem] h-[5rem]';
-  return (
-    <div
-      data-testid="loading-spinner"
-      className={`rounded-full border-4 border-transparent border-t-current animate-spin ${diameter}`}
-    >
-      <span className="sr-only">loading</span>
-    </div>
-  );
-};
+export const Spinner: React.FC<Props> = ({ size = 'lg' }) => (
+  <div
+    data-testid="loading-spinner"
+    className={`rounded-full border-4 border-transparent border-t-current animate-spin ${sizes[size]}`}
+  >
+    <span className="sr-only">loading</span>
+  </div>
+);
