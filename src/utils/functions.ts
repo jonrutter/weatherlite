46c8842e3ae032aef53036e812e8b8ff5594758b
Jon Rutter <contact@jonrutter.io>
2022-08-09 11:17:00 -0400

    Remove capitalize util function
    Use lodash instead, for simplicity

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,13 +1,3 @@
-export const capitalize = (str: string): string => {
-  if (typeof str !== 'string') {
-    console.warn('Bad input passed to capitalize()');
-    return '';
-  } else if (str.length < 2) return str.toUpperCase();
-  else {
-    return str[0].toUpperCase() + str.slice(1).toLowerCase();
-  }
-};
-
 export const toFixedZero = (num: number): number => {
   if (typeof num !== 'number') {
     console.warn('Bad input passed to toFixedZero()');
