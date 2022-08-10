834bb90dd939cc29922e38e2daa0fb1b9f67b40d
Jon Rutter <contact@jonrutter.io>
2022-08-10 12:48:19 -0400

    Convert msw files to TypeScript

--- src/app/mocks/handlers.ts
+++ src/app/mocks/handlers.ts
@@ -3,15 +3,18 @@ import { rest } from 'msw';
 import { mockLocationData } from './mock-data/mock-location-data';
 import { mockWeatherData } from './mock-data/mock-weather-data';
 
+// types
+import { WeatherData, GeoResponse } from '../../types';
+
 export const handlers = [
-  rest.get('/api/location', (req, res, ctx) => {
+  rest.get<{}, {}, GeoResponse>('/api/location', (req, res, ctx) => {
     return res(
       ctx.delay(2000),
       ctx.status(200),
       ctx.json({ ...mockLocationData })
     );
   }),
-  rest.get('/api/weather', (req, res, ctx) => {
+  rest.get<{}, {}, WeatherData>('/api/weather', (req, res, ctx) => {
     return res(
       ctx.delay(2000),
       ctx.status(200),
