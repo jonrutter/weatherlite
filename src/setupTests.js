5f8c941764920c4d4c63c28b89dd4d1086d22b17
Jon Rutter <contact@jonrutter.io>
2022-08-16 18:10:41 -0400

    Mock server in Jest
    - Create msw node server
    
    - Add server to tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,11 @@
 import '@testing-library/jest-dom';
 
-// mocks
+// jest mocks
 import './test/__mocks__/intersection-observer';
+
+// msw
+import { server } from './app/mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
