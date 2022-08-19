2f81b917b762c1fb0b534c1681c5528320eed24b
Jon Rutter <contact@jonrutter.io>
2022-08-19 12:18:18 -0400

    Clean up index.tsx
    - Remove unused code
    
    - Add some comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import { Provider } from 'react-redux';
-
 // fonts
-import '@fontsource/open-sans';
 import '@fontsource/nunito';
 import '@fontsource/inter';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/900.css';
 
-// import { worker } from './api/server';
-
-// import { store } from './app/store';
-
 // app
 import { App } from './app';
-// import { saveState } from './app/localStorage';
 
-// styles
+// global styles
 import './styles/index.css';
 
-// store.subscribe(() => {
-// saveState(store.getState().location);
-// });
-
-// async function start() {
-//   // Start our mock API server
-//   await worker.start({ onUnhandledRequest: 'bypass' });
-
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   );
-// }
-
-// start();
-
+// use msw mock server in development
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./app/mocks/browser');
   worker.start({
@@ -49,6 +22,7 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
+// render app to dom
 ReactDOM.render(
   <React.StrictMode>
     <App />
