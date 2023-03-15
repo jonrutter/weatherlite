ad85ba17569dc22b48e3781f408b28ef3c88d608
Jon Rutter <contact@jonrutter.io>
2023-03-15 17:40:17 -0400

    Add types to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     transitionDuration: {
       DEFAULT: '300ms',
