209c5f4e67b19da6fcc99d56020e3e10d0bebf03
Jon Rutter <contact@jonrutter.io>
2023-03-15 18:11:14 -0400

    Set viewport metadata in _app instead of _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/index.css';
 import type { AppProps } from 'next/app';
 import { Nunito, Inter } from 'next/font/google';
+import Head from 'next/head';
 
 // fonts
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
@@ -8,8 +9,13 @@ const nunito = Nunito({ subsets: ['latin'], variable: '--font-nunito' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} ${nunito.variable}`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={`${inter.variable} ${nunito.variable}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
